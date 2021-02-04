0e052f99174d854d5a937d8ab0a7910ac544a655
yanminghao <2540898831@qq.com>
2021-02-04 09:46:22 +0800

    循环代码

--- src/main/java/com/sirius/test/BaseDegsignTest.java
+++ src/main/java/com/sirius/test/BaseDegsignTest.java
@@ -11,13 +11,14 @@ public class BaseDegsignTest {
 
     //静态方法，连接字符串
     @Test
-    public void test01(){
+    public void test01() {
         String join = String.join("Ymh_Sirius", "hello", "JAVA 基础");
         System.out.println(join);
     }
+
     //String jdk11及其以上  重复输出字符串次数
     @Test
-    public void test02(){
+    public void test02() {
         String repeat = "Ymh".repeat(100);
         System.out.println(repeat);
 
@@ -25,7 +26,7 @@ public class BaseDegsignTest {
 
     //String jdk11及其以上  重复输出字符串次数
     @Test
-    public void test03(){
+    public void test03() {
         StringBuffer stringBuffer = new StringBuffer();
         System.out.println(stringBuffer);
         StringBuilder stringBuilder = new StringBuilder();
@@ -34,7 +35,7 @@ public class BaseDegsignTest {
 
 
     @Test
-    public void test04(){
+    public void test04() {
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("wHATE INS   123 qwe" +
 //                "" +
@@ -43,16 +44,31 @@ public class BaseDegsignTest {
 //        System.out.println(s);
 //        System.out.println(scanner.next());
 //        System.out.println(scanner.nextInt());
-        Console console = System.console(                                             );
+        Console console = System.console();
         System.out.println(console);
     }
-      @Test
-      public void test05() {
+
+    @Test
+    public void test05() {
         //格式化输出
         System.out.println(1000.0 / 3);
         System.out.printf("%8.2f\n", 10000.0 / 3);
     }
 
+    @Test
+    public void test06() {
+        //循环 while
+//        int i = 10;
+//        while (i < 0) {
+//            System.out.println("i:" + i);
+//            i--;
+//        }
+        System.out.println("—————————for—————");
+        for (int j = 10; j > 0; j--) {
+            System.out.println("i=" + j);
+        }
+
+    }
 
 
 }
